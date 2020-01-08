97eca89f586e601e93cc557d67171b3690c0c5f9
sina <sina.majdieh@gmail.com>
2020-01-08 21:02:58 +0330

    Accepts []float64 and []int

--- QuickSort.go
+++ QuickSort.go
@@ -37,17 +37,23 @@ func (q quick) sort(low int, high int) {
 }
 
 //Quick Sort Algorithm
-func QuickSort(Ascending bool, Array interface{}) {
+func QuickSort(Ascending bool, Array interface{}) []float64 {
 	q := quick{}
 	switch Array.(type) {
 	case []int:
-		intstofloats(Array.([]int) , q.array)
+		array := intstofloats(Array.([]int))
+		q.array = make([]float64, len(Array.([]int)))
+		copy(q.array, array)
 	case []float64:
-		q.array = Array.([]float64)
+		q.array = make([]float64, len(Array.([]float64)))
+		copy(q.array, Array.([]float64))
 	default:
 		fmt.Printf("Cannot sort type %T\n", Array)
+		return nil
 	}
 
 	q.ascending = Ascending
 	q.sort(0, len(q.array)-1)
+
+	return q.array
 }
